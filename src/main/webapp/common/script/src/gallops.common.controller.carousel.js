417bc914d95cc532cf40ae1292deb3ff1f7d8e03
dsah <dnsah@hotmail.com>
2016-03-27 21:26:29 +1100

    caraousel changes
    caraosel

--- src/main/webapp/common/script/src/gallops.common.controller.carousel.js
+++ src/main/webapp/common/script/src/gallops.common.controller.carousel.js
@@ -11,12 +11,45 @@ gallops.common.controller.CarouselController = ['$scope',
         $scope.myInterval = 2000;
         $scope.slides = [
             {
-                image: 'src/main/webapp/resources/images/Picture1.png',
+                image: 'src/main/webapp/resources/images/carousel/Picture1.png',
                 id:0
             },
             {
-                image: 'src/main/webapp/resources/images/picture2.png',
+                image: 'src/main/webapp/resources/images/carousel/adelaide.jpg',
                 id:1
+            },
+            {
+                image: 'src/main/webapp/resources/images/carousel/brisbane.png',
+                id:2
+            },
+            {
+                image: 'src/main/webapp/resources/images/carousel/canberra.jpg',
+                id:3
+            },
+            {
+                image: 'src/main/webapp/resources/images/carousel/NSW.jpg',
+                id:4
+            },
+            {
+                image: 'src/main/webapp/resources/images/carousel/macquarie.jpg',
+                id:5
+            }
+            ,
+            {
+                image: 'src/main/webapp/resources/images/carousel/nt.jpg',
+                id:6
+            },
+            {
+                image: 'src/main/webapp/resources/images/carousel/Perth.jpg',
+                id:7
+            },
+            {
+                image: 'src/main/webapp/resources/images/carousel/surf.jpg',
+                id:8
+            },
+            {
+                image: 'src/main/webapp/resources/images/carousel/Victoria.jpg',
+                id:9
             }
             
         ];
